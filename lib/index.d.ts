7d915e53eee2dba2a4214528cde89a2d1dfd06ee
Stefanuk12 <stefanukpadd@gmail.com>
2021-06-26 17:00:44 +0100

    Typings

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -7,7 +7,7 @@ declare namespace RAssets {
     function getCSRF(cookie: string): Promise<string | undefined>;
     function getRequestVerificationToken(cookie: string): Promise<string>;
     function uploadAudio(cookie: string, data: IAudioRequest): Promise<IAudioResponse>;
-    function upload(cookie: string, data: IAssetUploadRequest): Promise<string>;
+    function upload(cookie: string, data: IAssetUploadRequest): Promise<string | IAudioResponse>;
     function verifyAudio(cookie: string, data: IAudioVerifyRequest): Promise<IAudioVerifyResponse>;
 }
 export = RAssets;
