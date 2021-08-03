53d7896407ec9cd8e2776cdbc97d2b7d5ba0eab8
Stefanuk12 <stefanukpadd@gmail.com>
2021-08-03 21:17:08 +0100

    Minor type change

--- lib/interfaces/IAssetUploadRequest.d.ts
+++ lib/interfaces/IAssetUploadRequest.d.ts
@@ -1,11 +1,13 @@
 /// <reference types="node" />
+declare type AssetType = "Image" | "TeeShirt" | "Audio" | "Mesh" | "Lua" | "Hat" | "Place" | "Model" | "Shirt" | "Pants" | "Decal" | "Head" | "Face" | "Gear" | "Badge" | "Animation" | "Torso" | "RightArm" | "LeftArm" | "LeftLeg" | "RightLeg" | "Package" | "GamePass" | "Plugin" | "MeshPart" | "HairAccessory" | "FaceAccessory" | "NeckAccessory" | "ShoulderAccessory" | "FrontAccessory" | "BackAccessory" | "WaistAccessory" | "ClimbAnimation" | "DeathAnimation" | "FallAnimaiton" | "IdleAnimation" | "JumpAnimation" | "RunAnimation" | "SwimAnimation" | "WalkAnimation" | "PoseAnimation" | "EarAccessory" | "EyeAccessory" | "EmoteAnimation" | "Video";
 export interface IAssetUploadRequest {
     file: Buffer;
     assetid?: number;
-    assetType: "Image" | "TeeShirt" | "Audio" | "Mesh" | "Lua" | "Hat" | "Place" | "Model" | "Shirt" | "Pants" | "Decal" | "Head" | "Face" | "Gear" | "Badge" | "Animation" | "Torso" | "RightArm" | "LeftArm" | "LeftLeg" | "RightLeg" | "Package" | "GamePass" | "Plugin" | "MeshPart" | "HairAccessory" | "FaceAccessory" | "NeckAccessory" | "ShoulderAccessory" | "FrontAccessory" | "BackAccessory" | "WaistAccessory" | "ClimbAnimation" | "DeathAnimation" | "FallAnimaiton" | "IdleAnimation" | "JumpAnimation" | "RunAnimation" | "SwimAnimation" | "WalkAnimation" | "PoseAnimation" | "EarAccessory" | "EyeAccessory" | "EmoteAnimation" | "Video";
+    assetType: AssetType;
     name: string;
     description?: string;
     ispublic?: boolean;
     allowcomments?: boolean;
 }
+export {};
 //# sourceMappingURL=IAssetUploadRequest.d.ts.map
